77fb5efc37c599990a7a71db1ee83a96a0c1c88b
Cristobal Andres Contreras Orellana <ext_cristobal.contreras@cajalosandes.cl>
2023-01-21 03:42:37 -0300

    feature: add reference values

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -5,16 +5,25 @@ fn main() {
 
     io::stdin().read_line(&mut input);
 
-    println!("Input: {}", input);
+    borrow_string(&input);
+    own_string(input);
 
     // typed variables
-    let weight_on_earth = 72.0;
-    let weight_on_mars = calculate_weight_on_mars(weight_on_earth);
+    // let weight_on_earth = 72.0;
+    // let weight_on_mars = calculate_weight_on_mars(weight_on_earth);
 
     // cannot change the value of a variable once it is set, unless it is mutable
     // weight_on_mars = 72.0;
 
-    println!("Weight on Mars: {}kgen", weight_on_mars);
+    // println!("Weight on Mars: {}kgen", weight_on_mars);
+}
+
+fn borrow_string(s: &String) {
+    println!("{}", s);
+}
+
+fn own_string(s: String) {
+    println!("{}", s);
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
