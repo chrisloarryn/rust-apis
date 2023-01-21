97aef1fb5a630b863e7d2745591449d4e49dfe53
Cristobal Andres Contreras Orellana <ext_cristobal.contreras@cajalosandes.cl>
2023-01-21 03:30:00 -0300

    fix: error while building

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -1,17 +1,11 @@
 use std::io;
 
 fn main() {
-
     let mut input = String::new();
-    
-    let mut s1 = &mut input;
-    let s2 = &input;
-
-    println!("{} {}", s1, s2);
-
-    some_fn(&mut input);
 
     io::stdin().read_line(&mut input);
+
+    println!("Input: {}", input);
 
     // typed variables
     let weight_on_earth = 72.0;
@@ -25,9 +19,4 @@ fn main() {
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
     (weight / 9.81) * 3.711
-}
-
-
-fn some_fn(s: &mut String) {
-    s.push_str("hello");
 }
